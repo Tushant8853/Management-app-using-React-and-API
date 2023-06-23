71139d6f2ff834c25dea66aec1797d8dd6985594
Tushant Gupta <tushantgupta8853@gmail.com>
2023-06-23 20:20:23 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'; // Import custom CSS styles
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const fetchEmployees = async () => {
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=2');
+      setEmployees(response.data.data);
+    } catch (error) {
+      console.error('Error fetching employees:', error);
+    }
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredEmployees = employees.filter((employee) =>
+    employee.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search by first name"
+          value={searchTerm}
+          onChange={handleSearch}
+          className="search-input"
+        />
+      </div>
+      <ul className="employee-list">
+        {filteredEmployees.map((employee) => (
+          <li key={employee.id} className="employee-item">
+            <div className="employee-details">
+              <div className="employee-id">{employee.id}</div>
+              <img className="employee-avatar" src={employee.avatar} alt={employee.first_name} />
+              <div className="employee-name">{employee.first_name}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
